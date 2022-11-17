c4786c8bbb49e0ebfd614ead2965649edcbc2bcf
Nikol Nesic <mi18170@matf.bg.ac.rs>
2022-11-17 19:02:20 +0100

    feat:Implementation of class Treasure and class Wagon

--- TrafficExpress/headers/treasure.h
+++ TrafficExpress/headers/treasure.h
@@ -1,11 +1,31 @@
 #ifndef TREASURE_H
 #define TREASURE_H
 
+#include<string>
+enum class TreasureType{
+   SUITCASE,DIAMOND,MONEYBAG
+};
 
 class Treasure
 {
 public:
     Treasure();
+
+
+    Treasure(int value,TreasureType type);
+    Treasure(const Treasure&t);
+
+    Treasure& operator=(const Treasure&t);
+
+    std::string toString();
+    bool operator==(const Treasure&t);
+    bool operator!=(const Treasure&t);
+
+    TreasureType getType();
+    int getValue();
+private:
+    TreasureType m_type;
+    int m_value;
 };
 
 #endif // TREASURE_H
--- TrafficExpress/src/treasure.cpp
+++ TrafficExpress/src/treasure.cpp
@@ -1,6 +1,59 @@
 #include "../headers/treasure.h"
 
 Treasure::Treasure()
+    :m_type(TreasureType::MONEYBAG),m_value(250)
 {
 
 }
+
+Treasure::Treasure(int value, TreasureType type)
+    :m_type(type),m_value(value)
+{
+
+}
+
+Treasure::Treasure(const Treasure &t)
+    :m_type(t.m_type),m_value(t.m_value)
+{
+
+}
+
+Treasure &Treasure::operator=(const Treasure &t)
+{
+    auto tmp=Treasure(t);
+    std::swap(m_type,tmp.m_type);
+    std::swap(m_value,tmp.m_value);
+    return *this;
+}
+
+std::string Treasure::toString()
+{
+    switch(m_type){
+                case TreasureType::MONEYBAG:return "MONEYBAG:"+std::to_string(m_value);
+                case TreasureType::DIAMOND:return "DIAMOND:"+std::to_string(m_value);;
+                case TreasureType::SUITCASE:return "SUITCASE:"+std::to_string(m_value);;
+                default:return "UNKNOWN TREASURE TYPE";
+
+    }
+}
+
+bool Treasure::operator==(const Treasure &t)
+{
+    return m_type==t.m_type && m_value==t.m_value;
+}
+
+bool Treasure::operator!=(const Treasure &t)
+{
+    return !(m_type==t.m_type && m_value==t.m_value);
+}
+
+TreasureType Treasure::getType()
+{
+    return m_type;
+}
+
+int Treasure::getValue()
+{
+    return m_value;
+}
+
