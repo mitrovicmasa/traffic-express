03d5e1b0ca95de7daf7c37f92279553cc8831e90
AndreeaCatalina16 <andreeacitcauan@gmail.com>
2022-11-19 12:32:21 +0100

    feat: Implementation of class roundCards

--- TrafficExpress/headers/roundcards.h
+++ TrafficExpress/headers/roundcards.h
@@ -1,11 +1,75 @@
 #ifndef ROUNDCARDS_H
 #define ROUNDCARDS_H
 
+#include <iostream>
+#include <string>
+#include <vector>
 
-class RoundCards
-{
-public:
-    RoundCards();
+#include "card.h"
+
+enum class RoundCardType {
+    
+    THREE_TO_FOUR_PLAYERS,
+    FIVE_TO_SIX_PLAYERS,
+    TRAIN_STATION
+
+}; 
+
+enum class EventType {
+
+    ANGRY_MARSHAL,
+    SWIVEL_ARM,
+    BRACKING,
+    TAKE_IT_ALL,
+    PASSENGERS_REBELLION,
+    PICKPOCKETING,
+    MARSHALS_REVENGE,
+    HOSTAGE_TACKING_OF_THE_CONDUCTOR
+
 };
 
-#endif // ROUNDCARDS_H
+enum class MiniRoundType {
+
+    FACE_UP,
+    HIDDEN,
+    DOUBLE_CARDS,
+    OPPOSITE_DIRECTION
+
+};
+
+std::string toStringRoundCardType(const RoundCardType &card);
+std::string toStringEventType(const EventType &event);
+std::string toStringMiniRoundType(const MiniRoundType &miniRound);
+
+class RoundCards {
+
+public:
+    RoundCards(RoundCardType type, EventType event, const std::vector<MiniRoundType> &miniRound);
+    
+    RoundCardType type() const;
+    const std::vector<MiniRoundType> &miniRound() const;
+    EventType event() const;
+
+    void setType(RoundCardType &newType);
+    void setEvent(EventType &newEvent);
+    void setMiniRound(const std::vector<MiniRoundType> &newMiniRound);
+
+    std::string toString() const;
+
+    ~RoundCards() = default;
+
+    RoundCards(const RoundCards&);
+    void pushBackMiniRound(MiniRoundType type);
+
+
+private:
+
+    RoundCardType m_type;
+    EventType m_event;
+    std::vector<MiniRoundType> m_miniRound;
+
+};
+
+
+
+#endif //ROUNDCARDS_H
--- TrafficExpress/src/roundcards.cpp
+++ TrafficExpress/src/roundcards.cpp
@@ -1,6 +1,111 @@
 #include "../headers/roundcards.h"
 
-RoundCards::RoundCards()
+RoundCards::RoundCards(RoundCardType type, EventType event, const std::vector<MiniRoundType> &miniRound)
+    : m_type(type), 
+      m_event(event),
+      m_miniRound(miniRound)
+{}
+
+RoundCardType RoundCards::type() const
 {
+    return m_type;
+}
+
+EventType RoundCards::event() const 
+{
+    return m_event;
+}
+
+const std::vector<MiniRoundType> &RoundCards::miniRound() const
+{
+    return m_miniRound;
+}
+
+
+void RoundCards::setType(RoundCardType &newType) 
+{
+    m_type = newType;
+}
+
+void RoundCards::setEvent(EventType &newEvent) 
+{
+    m_event = newEvent;
+}
+
+void RoundCards::setMiniRound(const std::vector<MiniRoundType> &newMiniRound) 
+{
+    m_miniRound = newMiniRound;
+}
+
+std::string toStringRoundCardType(const RoundCardType &card)
+{
+    switch (card) {
+        case RoundCardType::THREE_TO_FOUR_PLAYERS:
+            return "Three to four players";
+        case RoundCardType::FIVE_TO_SIX_PLAYERS:
+            return "Five to six players";
+        case RoundCardType::TRAIN_STATION:
+            return "Train station";
+        default:
+            break;
+    }
+
+}
+
+std::string toStringEventType(const EventType &event) 
+{
+    switch (event)
+    {
+    case EventType::ANGRY_MARSHAL:
+        return "Angry Marshal";
+    case EventType::SWIVEL_ARM:
+        return "Swivel Arm";
+    case EventType::BRACKING:
+        return "Bracking";
+    case EventType::TAKE_IT_ALL:
+        return "Take it all!";
+    case EventType::PASSENGERS_REBELLION:
+        return "Passengers'Rebellion";
+    case EventType::PICKPOCKETING:
+        return "Pickpocketing";
+    case EventType::MARSHALS_REVENGE:
+        return "Marshal's Revenge";
+    case EventType::HOSTAGE_TACKING_OF_THE_CONDUCTOR:
+        return "Hostage-Taking of the Conductor";   
+    default:
+        break;
+    }
+
+}
+
+std::string toStringMiniRoundType(const MiniRoundType &miniRound) 
+{
+    switch (miniRound)
+    {
+    case MiniRoundType::HIDDEN:
+        return "Hidden!";
+    case MiniRoundType::DOUBLE_CARDS:
+        return "Double cards!";
+    case MiniRoundType::OPPOSITE_DIRECTION:
+        return "Opposite direction!";
+    case MiniRoundType::FACE_UP:
+        "Face up - default!";
+    
+    default:
+        break;
+    }
+
+}
+
+RoundCards::RoundCards(const RoundCards &r) 
+    :
+    m_type(r.m_type), 
+    m_event(r.m_event),
+    m_miniRound(r.m_miniRound)
+{}
+
+void RoundCards::pushBackMiniRound(MiniRoundType type)
+{
+    m_miniRound.push_back(type);
 
 }
