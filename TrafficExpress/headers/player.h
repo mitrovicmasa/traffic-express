e1f9185aa937f2b3832b30b3094b6ed52ad95bdd
Milica Vucic <vucic.milica99@gmail.com>
2022-11-19 19:20:41 +0100

    feat: Implementation of class Player

--- TrafficExpress/headers/player.h
+++ TrafficExpress/headers/player.h
@@ -1,11 +1,57 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <functional>
+
+#include <card.h>
+#include <treasure.h>
+#include <banditcard.h>
+#include <bulletcard.h>
 
 class Player
 {
 public:
     Player();
-};
+    Player(BanditType id, const std::vector<Card> &hand, const std::vector<Card> &deck,
+           const std::vector<BulletCard> &bulletDeck, int positionInTrain, bool roof, const std::vector<Treasure> treasure);
+    Player(const Player &player);
 
+    Player &operator=(const Player &player);
+
+    BanditType id() const;
+    const std::vector<Card> &hand() const;
+    const std::vector<Card> &deck() const;
+    const std::vector<BulletCard> &bulletCard() const;
+    int positionInTrain() const;
+    bool roof() const;
+    const std::vector<Treasure> &treasure() const;
+
+    void setId(BanditType &newId);
+    void setHand(const std::vector<Card> &newHand);
+    void setDeck(const std::vector<Card> &newDeck);
+    void setBulletDeck(const std::vector<BulletCard> &newBulletDeck);
+    void setPositionInTrain(int &newPositionInTrain);
+    void setRoof(bool &newRoof);
+    void setTreasure(std::vector<Treasure> newTreasure);
+
+    bool isBulletDeckEmpty() const;
+    int countAmountOfTreasure() const;
+
+    std::string toString() const;
+
+    ~Player() = default;
+
+private:
+    BanditType m_id;
+    std::vector<Card> m_hand;
+    std::vector<Card> m_deck;
+    std::vector<BulletCard> m_bulletDeck;
+    int m_positionInTrain;
+    bool m_roof;
+    std::vector<Treasure> m_treasure;
+};
 #endif // PLAYER_H
--- TrafficExpress/src/player.cpp
+++ TrafficExpress/src/player.cpp
@@ -1,6 +1,133 @@
 #include "../headers/player.h"
 
 Player::Player()
-{
+{}
 
+Player::Player(BanditType id, const std::vector<Card> &hand, const std::vector<Card> &deck,
+               const std::vector<BulletCard> &bulletDeck, int positionInTrain, bool roof, const std::vector<Treasure> treasure)
+    : m_id(id),
+      m_hand(hand),
+      m_deck(deck),
+      m_bulletDeck(bulletDeck),
+      m_positionInTrain(positionInTrain),
+      m_roof(roof),
+      m_treasure(treasure)
+{}
+
+Player::Player(const Player &player)
+    : m_id(player.m_id),
+      m_hand(player.m_hand),
+      m_deck(player.m_deck),
+      m_bulletDeck(player.m_bulletDeck),
+      m_positionInTrain(player.m_positionInTrain),
+      m_roof(player.m_roof),
+      m_treasure(player.m_treasure)
+{}
+
+
+Player &Player::operator=(const Player &player)
+{
+    auto tmp = Player(player);
+    std::swap(m_id, tmp.m_id);
+    std::swap(m_hand, tmp.m_hand);
+    std::swap(m_deck, tmp.m_deck);
+    std::swap(m_bulletDeck, tmp.m_bulletDeck);
+    std::swap(m_positionInTrain, tmp.m_positionInTrain);
+    std::swap(m_roof, tmp.m_roof);
+    std::swap(m_treasure, tmp.m_treasure);
+
+    return *this;
+}
+
+BanditType Player::id() const
+{
+    return m_id;
+}
+
+const std::vector<Card> &Player::hand() const
+{
+    return m_hand;
+}
+
+const std::vector<Card> &Player::deck() const
+{
+    return m_deck;
+}
+
+const std::vector<BulletCard> &Player::bulletCard() const
+{
+    return m_bulletDeck;
+}
+
+int Player::positionInTrain() const
+{
+    return m_positionInTrain;
+}
+
+bool Player::roof() const
+{
+    return m_roof;
+}
+
+const std::vector<Treasure> &Player::treasure() const
+{
+    return m_treasure;
+}
+
+void Player::setId(BanditType &newId)
+{
+    m_id = newId;
+}
+
+void Player::setHand(const std::vector<Card> &newHand)
+{
+    m_hand = newHand;
+}
+
+void Player::setDeck(const std::vector<Card> &newDeck)
+{
+    m_deck = newDeck;
+}
+
+void Player::setBulletDeck(const std::vector<BulletCard> &newBulletDeck)
+{
+    m_bulletDeck = newBulletDeck;
+}
+
+void Player::setPositionInTrain(int &newPositionInTrain)
+{
+    m_positionInTrain = newPositionInTrain;
+}
+
+void Player::setRoof(bool &newRoof)
+{
+    m_roof = newRoof;
+}
+
+void Player::setTreasure(std::vector<Treasure> newTreasure)
+{
+    m_treasure = newTreasure;
+}
+
+bool Player::isBulletDeckEmpty() const
+{
+    return m_bulletDeck.empty();
+}
+
+int Player::countAmountOfTreasure() const
+{
+    return std::accumulate(m_treasure.cbegin(), m_treasure.cend(), 0, [](int acc, auto treasure) { return acc + treasure.getValue(); });
+}
+
+std::string Player::toString() const
+{
+    std::string positionInWagon = m_roof ? "Bandit is on the roof" : "Bandit is in the wagon";
+    int currentAmountOfTreasure = countAmountOfTreasure();
+
+    return "Bandit: "  + ::toString(m_id) + "\n"
+         + "Position in train: " + std::to_string(m_positionInTrain) + "\n"
+         + "Current amount of treasure: " + std::to_string(currentAmountOfTreasure) + "\n"
+         + "Current number of cards in deck: " + std::to_string(m_deck.size()) + "\n"
+         + "Current number of bullets in bullet deck: " + std::to_string(m_bulletDeck.size()) + "\n"
+         + positionInWagon + "\n";
 }
