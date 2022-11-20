c3df09be7342c936944a3fc8c5106795d6159814
Vukasin Radic <vukasinradic96@hotmail.com>
2022-11-20 23:23:35 +0100

    feat: Implementation of class Game

--- TrafficExpress/headers/game.h
+++ TrafficExpress/headers/game.h
@@ -1,11 +1,62 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "card.h"
+#include "wagon.h"
+#include "player.h"
+#include "roundcard.h"
+#include "actioncard.h"
+
+#include <vector>
+#include <algorithm>
 
 class Game
 {
 public:
+
+    // Constructors
     Game();
+    Game(const std::vector<Player> &players, const std::vector<Wagon> &wagons, unsigned sheriffPosition,
+         const std::vector<RoundCard*> &rounds, const std::vector<ActionCard*> &cardsPlayed,
+         const std::vector<Treasure> &unusedTreasure, BanditType mostBulletsShot, BanditType richestPlayer);
+
+    //Get methods
+    const std::vector<Player> &players() const;
+    const std::vector<Wagon> &wagons() const;
+    const std::vector<RoundCard*> &rounds() const;
+    const std::vector<ActionCard*> &cardsPlayed() const;
+    const std::vector<Treasure> &unusedTreasure() const;
+    BanditType mostBulletsShot() const;
+    BanditType richestPlayer() const;
+
+    //Set methods
+    void setPlayers(const std::vector<Player> &newPlayers);
+    void setWagons(const std::vector<Wagon> &newWagons);
+    void setRounds(const std::vector<RoundCard*> &newRounds);
+    void setCardsPlayed(const std::vector<ActionCard*> &newCardsPlayed);
+    void setUnusedTreasure(const std::vector<Treasure> &newUnusedTreasure);
+    void setMostBulletsShot(BanditType newMostBulletsShot);
+    void setRichestPlayer(BanditType newRichestPlayer);
+
+    //Other methods
+
+    const std::vector<Card> &shuffleDeck(std::vector<Card*> &cards) const;
+    const std::vector<Card> &drawCards(unsigned) const;
+    std::vector<Card*> drawCards(unsigned numberOfCards, const Player &player) const;
+    std::vector<RoundCard*> selectRoundCards() const;
+    std::vector<Wagon> selectWagons() const;
+    void placeBanditInWagon(unsigned) const;
+
+private:
+
+    std::vector<Player> m_players;
+    std::vector<Wagon> m_wagons;
+    unsigned m_sheriffPosition;
+    std::vector<RoundCard*> m_rounds;
+    std::vector<ActionCard*> m_cardsPlayed;
+    std::vector<Treasure> m_unusedTreasure;
+    BanditType m_mostBulletsShot;
+    BanditType m_richestPlayer;
 };
 
 #endif // GAME_H
--- TrafficExpress/src/game.cpp
+++ TrafficExpress/src/game.cpp
@@ -4,3 +4,79 @@ Game::Game()
 {
 
 }
+
+Game::Game(const std::vector<Player> &players, const std::vector<Wagon> &wagons, unsigned sheriffPosition,
+           const std::vector<RoundCard *> &rounds, const std::vector<ActionCard *> &cardsPlayed,
+           const std::vector<Treasure> &unusedTreasure, BanditType mostBulletsShot, BanditType richestPlayer)
+{}
+
+const std::vector<Player> &Game::players() const
+{
+    return m_players;
+}
+
+const std::vector<Wagon> &Game::wagons() const
+{
+    return m_wagons;
+}
+
+const std::vector<RoundCard*> &Game::rounds() const
+{
+    return m_rounds;
+}
+
+const std::vector<ActionCard*> &Game::cardsPlayed() const
+{
+    return m_cardsPlayed;
+}
+
+const std::vector<Treasure> &Game::unusedTreasure() const
+{
+    return m_unusedTreasure;
+}
+
+BanditType Game::mostBulletsShot() const
+{
+    return m_mostBulletsShot;
+}
+
+BanditType Game::richestPlayer() const
+{
+    return m_richestPlayer;
+}
+
+void Game::setPlayers(const std::vector<Player> &newPlayers)
+{
+    m_players = newPlayers;
+}
+
+void Game::setWagons(const std::vector<Wagon> &newWagons)
+{
+    m_wagons = newWagons;
+}
+
+void Game::setRounds(const std::vector<RoundCard*> &newRounds)
+{
+    m_rounds = newRounds;
+}
+
+void Game::setCardsPlayed(const std::vector<ActionCard*> &newCardsPlayed)
+{
+    m_cardsPlayed = newCardsPlayed;
+}
+
+void Game::setUnusedTreasure(const std::vector<Treasure> &newUnusedTreasure)
+{
+    m_unusedTreasure = newUnusedTreasure;
+}
+
+void Game::setMostBulletsShot(BanditType newMostBulletsShot)
+{
+    m_mostBulletsShot = newMostBulletsShot;
+}
+
+void Game::setRichestPlayer(BanditType newRichestPlayer)
+{
+    m_richestPlayer = newRichestPlayer;
+}
+
